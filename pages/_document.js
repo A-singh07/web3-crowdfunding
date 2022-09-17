c9e4a376ae3e52afd82ddf3974339a14bd1e4940
Aman Singh <imaman07singh@gmail.com>
2022-09-17 20:48:48 +0530

    font rel=stylesheet

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
           {/* -- Fonts -- */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet preload prefetch" as="style" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
 
         </Head>
         <body>
